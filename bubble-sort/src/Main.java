5207b78068b9c901161aa164916d576febe89ec1
ggrigorov <g.grigorov@blubito.com>
2024-04-01 10:46:00 +0300

    Added comparison between normal bubble sort and optimized bubble sort

--- bubble-sort/src/Main.java
+++ bubble-sort/src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Random;
 
 // Simplest sorting algorithm
 // Not suitable for large data sets
@@ -8,9 +9,31 @@ import java.util.Arrays;
 // It is stable
 public class Main {
     public static void main(String[] args) {
-        int[] arr = { 5, 4, 2 , 6, 1};
+        int[] arr = generateUnsortedIntegers(100000);
+        int[] arrCopy = Arrays.copyOf(arr, arr.length);
+
+
+        var startOptimizedSort = System.currentTimeMillis();
         optimizedBubbleSort(arr);
+        var endOptimizedSort = System.currentTimeMillis() - startOptimizedSort;
+        System.out.print("Result Optimized Sort: ");
         Arrays.stream(arr).forEach(e -> System.out.printf("%d ", e));
+        System.out.println();
+
+
+        var startSort = System.currentTimeMillis();
+        bubbleSort(arrCopy);
+        var endSort = System.currentTimeMillis() - startSort;
+        System.out.print("Result Normal Sort: ");
+        Arrays.stream(arrCopy).forEach(e -> System.out.printf("%d ", e));
+        System.out.println();
+
+        System.out.println("------------------------------------------");
+        System.out.printf("Optimized Time: %d ms\n", endOptimizedSort);
+        System.out.printf("Normal Time: %d ms\n", endSort);
+        System.out.println("------------------------------------------");
+
+
     }
 
     //TODO: compare both algorithms
@@ -43,5 +66,14 @@ public class Main {
                 }
             }
         }
+    }
+
+    private static int[] generateUnsortedIntegers(int size) {
+        int[] arr = new int[size];
+        Random random = new Random();
+        for (int i = 0; i < size; i++) {
+            arr[i] = random.nextInt();
+        }
+        return arr;
     }
 }
